a1e46d0510f2a038a4ccec051c0bea6c7371fe33
jamashita <jamashita@arachnid.xyz>
2019-01-18 23:12:49 +0900

    StatsItemRepository

---
+++ src/veau-repository/StatsItemRepository.ts
@@ -0,0 +1,60 @@
+import {VeauDB} from '../veau-infrastructure/VeauDB';
+import {CaptionID} from '../veau-vo/CaptionID';
+import {StatsItem, StatsItemRow} from '../veau-vo/StatsItem';
+
+export class StatsItemRepository implements IStatsItemRepository {
+  private static instance: StatsItemRepository = new StatsItemRepository();
+
+  public static getInstance(): StatsItemRepository {
+    return StatsItemRepository.instance;
+  }
+
+  private constructor() {
+  }
+
+  public async findByCaptionID(captionID: CaptionID): Promise<Map<string, Array<StatsItem>>> {
+    const query = `SELECT
+      R1.stats_id AS statsID,
+      R1.as_of AS asOf,
+      R1.value
+      FROM stats_items R1
+      INNER JOIN stats R2
+      USING(stats_id)
+      WHERE R2.caption_id = :captionID;`;
+
+    const statsItems: Array<StatsItemRow> = await VeauDB.query(query, [
+      {
+        captionID: captionID.get()
+      }
+    ]);
+
+    const itemMap: Map<string, Array<StatsItem>> = new Map();
+
+    statsItems.forEach((item) => {
+      const {
+        statsID,
+        asOf,
+        value
+      } = item;
+
+      const statsItem: StatsItem = StatsItem.of(asOf, value);
+      const items: Array<StatsItem> | undefined = itemMap.get(statsID);
+
+      if(items) {
+        items.push(statsItem);
+        return;
+      }
+
+      itemMap.set(statsID, [
+        statsItem
+      ]);
+    });
+
+    return itemMap;
+  }
+}
+
+export interface IStatsItemRepository {
+
+  findByCaptionID(captionID: CaptionID): Promise<Map<string, Array<StatsItem>>>;
+}
---
+++ src/veau-repository/__test__/StatsItemRepository.spec.ts
@@ -0,0 +1,72 @@
+import 'jest';
+import * as sinon from 'sinon';
+import {VeauDB} from '../../veau-infrastructure/VeauDB';
+import {CaptionID} from '../../veau-vo/CaptionID';
+import {StatsItem} from '../../veau-vo/StatsItem';
+import {UUID} from '../../veau-vo/UUID';
+import {StatsItemRepository} from '../StatsItemRepository';
+
+describe('StatsItemRepository', () => {
+  it('findByCaptionID', async () => {
+    const stub = sinon.stub();
+    VeauDB.query = stub;
+    stub.returns([
+      {
+        statsID: '98d1e9b5-6b18-44de-b615-d8016f49977d',
+        asOf: '2000-01-01',
+        value: 1
+      },
+      {
+        statsID: '5318ad74-f15f-4835-9fd7-890be4cce933',
+        asOf: '2001-01-01',
+        value: 11
+      },
+      {
+        statsID: '98d1e9b5-6b18-44de-b615-d8016f49977d',
+        asOf: '2000-01-02',
+        value: 2
+      },
+      {
+        statsID: '5318ad74-f15f-4835-9fd7-890be4cce933',
+        asOf: '2001-01-02',
+        value: 12
+      },
+      {
+        statsID: '98d1e9b5-6b18-44de-b615-d8016f49977d',
+        asOf: '2000-01-03',
+        value: 3
+      }
+    ]);
+
+    const statsItemRepository: StatsItemRepository = StatsItemRepository.getInstance();
+    const items: Map<string, Array<StatsItem>> = await statsItemRepository.findByCaptionID(CaptionID.of(UUID.of('d4703058-a6ff-420b-95b2-4475beba9027')));
+
+    const year2001: Array<StatsItem> | undefined = items.get('5318ad74-f15f-4835-9fd7-890be4cce933');
+
+    if (year2001) {
+      expect(year2001.length).toEqual(2);
+      expect(year2001[0].getAsOf()).toEqual('2001-01-01');
+      expect(year2001[0].getValue()).toEqual(11);
+      expect(year2001[1].getAsOf()).toEqual('2001-01-02');
+      expect(year2001[1].getValue()).toEqual(12);
+    }
+    else {
+      fail();
+    }
+
+    const year2000: Array<StatsItem> | undefined = items.get('98d1e9b5-6b18-44de-b615-d8016f49977d');
+
+    if (year2000) {
+      expect(year2000.length).toEqual(3);
+      expect(year2000[0].getAsOf()).toEqual('2000-01-01');
+      expect(year2000[0].getValue()).toEqual(1);
+      expect(year2000[1].getAsOf()).toEqual('2000-01-02');
+      expect(year2000[1].getValue()).toEqual(2);
+      expect(year2000[2].getAsOf()).toEqual('2000-01-03');
+      expect(year2000[2].getValue()).toEqual(3);
+    }
+    else {
+      fail();
+    }
+  });
+});
