a9dab951446bf451b3ffa47d3ca4fdd8e470e144
jamashita <jamashita@arachnid.xyz>
2019-01-18 16:14:07 +0900

    StateItem

---
+++ src/veau-vo/StatsItem.ts
@@ -0,0 +1,59 @@
+import {ValueObject} from './ValueObject';
+
+export type StatsItemJSON = {
+  asOf: string;
+  value: number;
+};
+
+export class StatsItem extends ValueObject {
+  private asOf: string;
+  private value: number;
+
+  public static of(asOf: string, value: number): StatsItem {
+    return new StatsItem(asOf, value);
+  }
+
+  private constructor(asOf: string, value: number) {
+    super();
+    this.asOf = asOf;
+    this.value = value;
+  }
+
+  public getAsOf(): string {
+    return this.asOf;
+  }
+
+  public getValue(): number {
+    return this.value;
+  }
+
+  public equals(other: StatsItem): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (this.asOf !== other.getAsOf()) {
+      return false;
+    }
+    if (this.value !== other.getValue()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public toJSON(): StatsItemJSON {
+    const {
+      asOf,
+      value
+    } = this;
+
+    return {
+      asOf,
+      value
+    };
+  }
+
+  public toString(): string {
+    return `${this.asOf} : ${this.value}`;
+  }
+}
