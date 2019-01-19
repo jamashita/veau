13c74603de5a70120e0befb41268f8659b81d425
jamashita <jamashita@arachnid.xyz>
2019-01-19 18:36:56 +0900

    AccountID

---
+++ src/veau-vo/VeauAccountID.ts
@@ -0,0 +1,33 @@
+import {ValueObject} from './ValueObject';
+
+export class VeauAccountID extends ValueObject {
+  private id: number;
+
+  public static of(id: number): VeauAccountID {
+    return new VeauAccountID(id);
+  }
+
+  private constructor(id: number) {
+    super();
+    this.id = id;
+  }
+
+  public get(): number {
+    return this.id;
+  }
+
+  public equals(other: VeauAccountID): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (this.id === other.get()) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public toString(): string {
+    return this.id.toString();
+  }
+}
---
+++ src/veau-vo/__test__/VeauAccountID.spec.ts
@@ -0,0 +1,14 @@
+import 'jest';
+import {VeauAccountID} from '../VeauAccountID';
+
+describe('VeauAccountID', () => {
+  it('equals', () => {
+    const account1: VeauAccountID = VeauAccountID.of(1);
+    const account2: VeauAccountID = VeauAccountID.of(2);
+    const account3: VeauAccountID = VeauAccountID.of(1);
+
+    expect(account1.equals(account1)).toEqual(true);
+    expect(account1.equals(account2)).toEqual(false);
+    expect(account1.equals(account3)).toEqual(true);
+  });
+});
