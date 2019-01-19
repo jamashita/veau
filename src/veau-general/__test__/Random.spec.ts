b1631a52e10a89014ea65f35fd86717ad535d27c
jamashita <jamashita@arachnid.xyz>
2019-01-20 00:29:53 +0900

    random test

---
+++ src/veau-general/__test__/Random.spec.ts
@@ -0,0 +1,23 @@
+import 'jest';
+import {Random} from '../Random';
+
+describe('Random', () => {
+  it('string: length is fixed', () => {
+    const length = 10;
+    for (let i = 0; i < 100000; i++) {
+      const str: string = Random.string(length);
+      expect(str.length).toEqual(length);
+    }
+  });
+
+  it('integer: value is over min and under max', () => {
+    const min = 0;
+    const max = 100;
+    for (let i = 0; i < 100000; i++) {
+      const value: number = Random.integer(min, max);
+
+      expect(value <= max).toEqual(true);
+      expect(min <= value).toEqual(true);
+    }
+  });
+});
