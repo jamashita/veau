8d05b6402c08a9b3005dd4d6c30be1e7c4829771
jamashita <jamashita@arachnid.xyz>
2019-01-21 18:01:03 +0900

    findByPage

--- src/veau-controller/api/Stats.ts
+++ src/veau-controller/api/Stats.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import { INTERNAL_SERVER_ERROR, NOT_FOUND, PRECONDITION_FAILED } from 'http-status';
 import * as log4js from 'log4js';
 import { StatsJSON } from '../../veau-entity/Stats';
+import { StatsOverviewJSON } from '../../veau-entity/StatsOverview';
 import { NoSuchElementError } from '../../veau-general/NoSuchElementError';
 import { IStatsUsecase, StatsUsecase } from '../../veau-usecase/StatsUsecase';
 import { UUID } from '../../veau-vo/UUID';
@@ -9,6 +10,25 @@ import { UUID } from '../../veau-vo/UUID';
 const router: express.Router = express.Router();
 const logger: log4js.Logger = log4js.getLogger();
 const statsUsecase: IStatsUsecase = StatsUsecase.getInstance();
+
+router.get('/overview/:page(\\d+)', async (req: express.Request, res: express.Response) => {
+  const page: number = Number(req.params.page);
+
+  if (isNaN(page)) {
+    res.sendStatus(PRECONDITION_FAILED);
+    return;
+  }
+
+  try {
+    const statsOverviews: Array<StatsOverviewJSON> = await statsUsecase.findByPage(page);
+
+    res.send(statsOverviews);
+  }
+  catch (err) {
+    logger.error(err.message);
+    res.sendStatus(INTERNAL_SERVER_ERROR);
+  }
+});
 
 router.get('/:statsID', async (req: express.Request, res: express.Response) => {
   const statsID: string = req.params.statsID;
--- src/veau-usecase/StatsUsecase.ts
+++ src/veau-usecase/StatsUsecase.ts
@@ -1,9 +1,12 @@
 import { Stats, StatsJSON } from '../veau-entity/Stats';
+import { StatsOverview, StatsOverviewJSON } from '../veau-entity/StatsOverview';
+import { IStatsOverviewRepository, StatsOverviewRepository } from '../veau-repository/StatsOverviewRepository';
 import { IStatsRepository, StatsRepository } from '../veau-repository/StatsRepository';
 import { StatsID } from '../veau-vo/StatsID';
 import { UUID } from '../veau-vo/UUID';
 
 const statsRepository: IStatsRepository = StatsRepository.getInstance();
+const statsOverviewRepository: IStatsOverviewRepository = StatsOverviewRepository.getInstance();
 
 export class StatsUsecase implements IStatsUsecase {
   private static instance: StatsUsecase = new StatsUsecase();
@@ -20,9 +23,19 @@ export class StatsUsecase implements IStatsUsecase {
 
     return stats.toJSON();
   }
+
+  public async findByPage(page: number): Promise<Array<StatsOverviewJSON>> {
+    const statsOverviews: Array<StatsOverview> = await statsOverviewRepository.findByPage(page);
+
+    return statsOverviews.map<StatsOverviewJSON>((statsOverview: StatsOverview) => {
+      return statsOverview.toJSON();
+    });
+  }
 }
 
 export interface IStatsUsecase {
 
   findByStatsID(statsID: string): Promise<StatsJSON>;
+
+  findByPage(page: number): Promise<Array<StatsOverviewJSON>>;
 }
