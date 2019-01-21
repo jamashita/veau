3d2924bcbd5bfacaa3be0419565dddea67d06f90
jamashita <jamashita@arachnid.xyz>
2019-01-22 01:28:32 +0900

    mysql scheme change

--- docker-compose/development/mysql/initdata/veau.sql
+++ docker-compose/development/mysql/initdata/veau.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 8.0.13)
 # Database: veau
-# Generation Time: 2019-01-20 18:52:17 +0000
+# Generation Time: 2019-01-21 16:27:53 +0000
 # ************************************************************
 
 
@@ -510,13 +510,16 @@ CREATE TABLE `stats` (
   `stats_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `language_id` tinyint(3) unsigned NOT NULL,
   `region_id` smallint(5) unsigned NOT NULL,
+  `term_id` tinyint(3) unsigned NOT NULL,
   `name` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`stats_id`),
   KEY `language_id` (`language_id`),
   KEY `region_id` (`region_id`),
-  CONSTRAINT `captions_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `languages` (`language_id`),
-  CONSTRAINT `captions_ibfk_2` FOREIGN KEY (`region_id`) REFERENCES `regions` (`region_id`)
+  KEY `term_id` (`term_id`),
+  CONSTRAINT `stats_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `languages` (`language_id`),
+  CONSTRAINT `stats_ibfk_2` FOREIGN KEY (`region_id`) REFERENCES `regions` (`region_id`),
+  CONSTRAINT `stats_ibfk_3` FOREIGN KEY (`term_id`) REFERENCES `terms` (`term_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
@@ -529,15 +532,12 @@ DROP TABLE IF EXISTS `stats_items`;
 CREATE TABLE `stats_items` (
   `stats_item_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `stats_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `term_id` tinyint(3) unsigned NOT NULL,
   `name` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `unit` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `seq` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY (`stats_item_id`),
   KEY `stats_id` (`stats_id`,`seq`),
-  KEY `term_id` (`term_id`),
-  CONSTRAINT `stats_items_ibfk_1` FOREIGN KEY (`stats_id`) REFERENCES `stats` (`stats_id`),
-  CONSTRAINT `stats_items_ibfk_2` FOREIGN KEY (`term_id`) REFERENCES `terms` (`term_id`)
+  CONSTRAINT `stats_items_ibfk_1` FOREIGN KEY (`stats_id`) REFERENCES `stats` (`stats_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
