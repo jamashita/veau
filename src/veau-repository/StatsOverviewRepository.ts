8ce96c2341412c8adb639066700b7f0d163ee26d
jamashita <jamashita@arachnid.xyz>
2019-01-21 17:37:30 +0900

    StatsOverviewRepository

---
+++ src/veau-repository/StatsOverviewRepository.ts
@@ -0,0 +1,49 @@
+import { StatsOverview, StatsOverviewRow } from '../veau-entity/StatsOverview';
+import { StatsOverviewFactory } from '../veau-factory/StatsOverviewFactory';
+import { VeauMySQL } from '../veau-infrastructure/VeauMySQL';
+
+const statsOverviewFactory: StatsOverviewFactory = StatsOverviewFactory.getInstance();
+const LIMIT: number = 40;
+
+export class StatsOverviewRepository implements IStatsOverviewRepository {
+  private static instance: StatsOverviewRepository = new StatsOverviewRepository();
+
+  public static getInstance(): StatsOverviewRepository {
+    return StatsOverviewRepository.instance;
+  }
+
+  private constructor() {
+  }
+
+  public async findByPage(page: number): Promise<Array<StatsOverview>> {
+    const query: string = `SELECT
+      R1.stats_id AS statsID,
+      R2.iso639,
+      R3.iso3166,
+      R1.name,
+      R1.updated_at AS updatedAt
+      FROM stats R1
+      INNER JOIN languages R2
+      USING(language_id)
+      INNER JOIN regions R3
+      USING(region_id)
+      LIMIT :limit
+      OFFSET :offset;`;
+
+    const statsOverviewRows: Array<StatsOverviewRow> = await VeauMySQL.query(query, [
+      {
+        limit: LIMIT,
+        offset: (page - 1) * LIMIT
+      }
+    ]);
+
+    return statsOverviewRows.map<StatsOverview>((statsOverviewRow: StatsOverviewRow) => {
+      return statsOverviewFactory.fromRow(statsOverviewRow);
+    });
+  }
+}
+
+export interface IStatsOverviewRepository {
+
+  findByPage(page: number): Promise<Array<StatsOverview>>;
+}
---
+++ src/veau-repository/__test__/StatsOverviewRepository.spec.ts
@@ -0,0 +1,33 @@
+/* tslint:disable */
+import 'jest';
+import { SinonStub } from 'sinon';
+import * as sinon from 'sinon';
+import { StatsOverview } from '../../veau-entity/StatsOverview';
+import { VeauMySQL } from '../../veau-infrastructure/VeauMySQL';
+import { StatsOverviewRepository } from '../StatsOverviewRepository';
+
+describe('StatsOverviewRepository', () => {
+  it('findByPage', async () => {
+    const stub: SinonStub = sinon.stub();
+    VeauMySQL.query = stub;
+    stub.returns([
+      {
+        statsID: '1a607fff-12c2-4d8d-a55a-75fa23971393',
+        iso639: 'ab',
+        iso3166: 'AFG',
+        name: 'stats overview',
+        updatedAt: new Date(2000, 0, 1)
+      }
+    ]);
+
+    const statsOverviewRepository: StatsOverviewRepository = StatsOverviewRepository.getInstance();
+    const statsOverviews: Array<StatsOverview> = await statsOverviewRepository.findByPage(0);
+
+    expect(statsOverviews.length).toEqual(1);
+    expect(statsOverviews[0].getStatsID().get().get()).toEqual('1a607fff-12c2-4d8d-a55a-75fa23971393');
+    expect(statsOverviews[0].getISO639().get()).toEqual('ab');
+    expect(statsOverviews[0].getISO3166().get()).toEqual('AFG');
+    expect(statsOverviews[0].getName()).toEqual('stats overview');
+    expect(statsOverviews[0].getUpdatedAt().toDate().getTime()).toEqual(new Date(2000, 0, 1).getTime());
+  });
+});
