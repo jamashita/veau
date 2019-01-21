e3864c80ce921844cc99b59342d7fe4150c78ea3
jamashita <jamashita@arachnid.xyz>
2019-01-21 22:26:29 +0900

    LocaleRepository

---
+++ src/veau-repository/LocaleRepository.ts
@@ -0,0 +1,58 @@
+import { NoSuchElementError } from '../veau-general/NoSuchElementError';
+import { ISO3166 } from '../veau-vo/ISO3166';
+import { ISO639 } from '../veau-vo/ISO639';
+import { Language } from '../veau-vo/Language';
+import { Region } from '../veau-vo/Region';
+
+export class LocaleRepository implements ILocaleRepository {
+  private languages: Array<Language>;
+  private regions: Array<Region>;
+
+  public static getInstance(languages: Array<Language>, regions: Array<Region>): LocaleRepository {
+    return new LocaleRepository(languages, regions);
+  }
+
+  private constructor(languages: Array<Language>, regions: Array<Region>) {
+    this.languages = languages;
+    this.regions = regions;
+  }
+
+  public findByISO639(iso639: ISO639): Language {
+    const languages: Array<Language> = this.languages.filter((language: Language) => {
+      if (language.getISO639().equals(iso639)) {
+        return true;
+      }
+
+      return false;
+    });
+
+    if (languages.length === 0) {
+      throw new NoSuchElementError(iso639.toString());
+    }
+
+    return languages[0];
+  }
+
+  public findByISO3166(iso3166: ISO3166): Region {
+    const regions: Array<Region> = this.regions.filter((region: Region) => {
+      if (region.getISO3166().equals(iso3166)) {
+        return true;
+      }
+
+      return false;
+    });
+
+    if (regions.length === 0) {
+      throw new NoSuchElementError(iso3166.toString());
+    }
+
+    return regions[0];
+  }
+}
+
+export interface ILocaleRepository {
+
+  findByISO639(iso639: ISO639): Language;
+
+  findByISO3166(iso3166: ISO3166): Region;
+}
---
+++ src/veau-repository/__test__/LocaleRepository.spec.ts
@@ -0,0 +1,100 @@
+/* tslint:disable */
+import 'jest';
+import { NoSuchElementError } from '../../veau-general/NoSuchElementError';
+import { ISO3166 } from '../../veau-vo/ISO3166';
+import { ISO639 } from '../../veau-vo/ISO639';
+import { Language } from '../../veau-vo/Language';
+import { LanguageID } from '../../veau-vo/LanguageID';
+import { Region } from '../../veau-vo/Region';
+import { RegionID } from '../../veau-vo/RegionID';
+import { LocaleRepository } from '../LocaleRepository';
+
+describe('LocaleRepository', () => {
+  it('findByISO639', () => {
+    const iso6391: ISO639 = ISO639.of('ab');
+    const iso6392: ISO639 = ISO639.of('aa');
+    const iso6393: ISO639 = ISO639.of('af');
+    const language1: Language = Language.of(LanguageID.of(1), 'language1', 'language1', iso6391);
+    const language2: Language = Language.of(LanguageID.of(2), 'language2', 'language2', iso6392);
+    const language3: Language = Language.of(LanguageID.of(3), 'language3', 'language3', iso6393);
+
+    const localeRepository: LocaleRepository = LocaleRepository.getInstance(
+      [
+      language1,
+      language2,
+      language3
+    ],
+      [
+      ]
+    );
+
+    const language: Language = localeRepository.findByISO639(iso6393);
+
+    expect(language.equals(language3)).toEqual(true);
+  });
+
+  it('findByISO639: throws error', () => {
+    const iso6391: ISO639 = ISO639.of('ab');
+    const iso6392: ISO639 = ISO639.of('aa');
+    const iso6393: ISO639 = ISO639.of('af');
+    const language1: Language = Language.of(LanguageID.of(1), 'language1', 'language1', iso6391);
+    const language2: Language = Language.of(LanguageID.of(2), 'language2', 'language2', iso6392);
+
+    const localeRepository: LocaleRepository = LocaleRepository.getInstance(
+      [
+        language1,
+        language2
+      ],
+      [
+      ]
+    );
+
+    expect(() => {
+      localeRepository.findByISO639(iso6393);
+    }).toThrow(NoSuchElementError);
+  });
+
+  it('findByISO3166', () => {
+    const iso31661: ISO3166 = ISO3166.of('AFG');
+    const iso31662: ISO3166 = ISO3166.of('ALB');
+    const iso31663: ISO3166 = ISO3166.of('DZA');
+    const region1: Region = Region.of(RegionID.of(1), 'region1', iso31661);
+    const region2: Region = Region.of(RegionID.of(2), 'region2', iso31662);
+    const region3: Region = Region.of(RegionID.of(3), 'region3', iso31663);
+
+    const localeRepository: LocaleRepository = LocaleRepository.getInstance(
+      [
+      ],
+      [
+        region1,
+        region2,
+        region3
+      ]
+    );
+
+    const region: Region = localeRepository.findByISO3166(iso31662);
+
+    expect(region.equals(region2)).toEqual(true);
+  });
+
+  it('findByISO3166: throws error', () => {
+    const iso31661: ISO3166 = ISO3166.of('AFG');
+    const iso31662: ISO3166 = ISO3166.of('ALB');
+    const iso31663: ISO3166 = ISO3166.of('DZA');
+    const region1: Region = Region.of(RegionID.of(1), 'region1', iso31661);
+    const region2: Region = Region.of(RegionID.of(2), 'region2', iso31662);
+
+    const localeRepository: LocaleRepository = LocaleRepository.getInstance(
+      [
+      ],
+      [
+        region1,
+        region2
+      ]
+    );
+
+    expect(() => {
+      localeRepository.findByISO3166(iso31663);
+    }).toThrow(NoSuchElementError);
+  });
+});
