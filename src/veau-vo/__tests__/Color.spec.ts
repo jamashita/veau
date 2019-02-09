4c1357b7f6b9b0b648a7191b39937119d3fb0710
jamashita <jamashita@arachnid.xyz>
2019-02-09 21:36:16 +0900

    define color

--- src/veau-vo/__tests__/Color.spec.ts
+++ src/veau-vo/__tests__/Color.spec.ts
@@ -4,24 +4,12 @@ import { Color } from '../Color';
 
 describe('Color', () => {
   it('equals', () => {
-    const color1: Color = Color.of(1, 2, 3);
-    const color2: Color = Color.of(4, 2, 3);
-    const color3: Color = Color.of(1, 5, 3);
-    const color4: Color = Color.of(1, 2, 6);
-    const color5: Color = Color.of(4, 5, 3);
-    const color6: Color = Color.of(1, 5, 6);
-    const color7: Color = Color.of(4, 2, 6);
-    const color8: Color = Color.of(4, 5, 6);
-    const color9: Color = Color.of(1, 2, 3);
+    const color1: Color = Color.of('#ffffff');
+    const color2: Color = Color.of('#fcfcfc');
+    const color3: Color = Color.of('#ffffff');
 
     expect(color1.equals(color1)).toEqual(true);
     expect(color1.equals(color2)).toEqual(false);
-    expect(color1.equals(color3)).toEqual(false);
-    expect(color1.equals(color4)).toEqual(false);
-    expect(color1.equals(color5)).toEqual(false);
-    expect(color1.equals(color6)).toEqual(false);
-    expect(color1.equals(color7)).toEqual(false);
-    expect(color1.equals(color8)).toEqual(false);
-    expect(color1.equals(color9)).toEqual(true);
+    expect(color1.equals(color3)).toEqual(true);
   });
 });
