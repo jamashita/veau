4ad67112f8b8247249bf79265ef5ac1aebfb1433
jamashita <jamashita@arachnid.xyz>
2019-02-11 01:30:00 +0900

    add pub sub

--- src/veau-general/Redis/Redis.ts
+++ src/veau-general/Redis/Redis.ts
@@ -14,11 +14,6 @@ export class Redis {
   public constructor(config: IORedis.RedisOptions) {
     const client: IORedis.Redis = new IORedis(config);
 
-    client.on('error', (err: any) => {
-      console.log('REDIS ERROR');
-      console.log(err);
-    });
-
     this.hash = new RedisHash(client);
     this.set = new RedisSet(client);
     this.list = new RedisList(client);
@@ -71,5 +66,21 @@ export class Redis {
 
       return true;
     });
+  }
+
+  public subscribe(channel: string): any {
+    return this.client.subscribe(channel);
+  }
+
+  public unsubscribe(channel: string): any {
+    return this.client.unsubscribe(channel);
+  }
+
+  public publish(channel: string, message: string): Promise<number> {
+    return this.client.publish(channel, message);
+  }
+
+  public onMessage(callback: (channel: string, message: string) => void): void {
+    this.client.on('message', callback);
   }
 }
--- src/veau-infrastructure/VeauRedis.ts
+++ src/veau-infrastructure/VeauRedis.ts
@@ -1,5 +1,5 @@
 import * as config from 'config';
-import * as redis from 'redis';
+import * as IORedis from 'ioredis';
 import { Redis } from '../veau-general/Redis/Redis';
 
-export const VeauRedis: Redis = new Redis(config.get<redis.ClientOpts>('redis'));
+export const VeauRedis: Redis = new Redis(config.get<IORedis.RedisOptions>('redis'));
