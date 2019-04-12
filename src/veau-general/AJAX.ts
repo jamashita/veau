a4c9fdad81339782ef69e4e99b69a729b04fe711
jamashita <jamashita@arachnid.xyz>
2019-04-12 23:11:26 +0900

    change the return value of AJAX.put and AJAX.delete (request.Request => AJAXRequest<T>)

--- src/veau-general/AJAX.ts
+++ src/veau-general/AJAX.ts
@@ -49,28 +49,44 @@ export class AJAX {
     });
   }
 
-  public static put(url: string, payload: any): Promise<request.Response> {
-    return new Promise<request.Response>((resolve: (value: request.Response) => void, reject: (reason: any) => void): void => {
+  public static put<T>(url: string, payload: any): Promise<AJAXResponse<T>> {
+    return new Promise<AJAXResponse<T>>((resolve: (value: AJAXResponse<T>) => void, reject: (reason: any) => void): void => {
       request.put(url).send(payload).end((err: any, res: request.Response) => {
           if (err) {
             reject(err);
             return;
           }
 
-          resolve(res);
+          const {
+            status,
+            body
+          } = res;
+
+          resolve({
+            status,
+            body
+          });
         });
     });
   }
 
-  public static delete(url: string): Promise<request.Response> {
-    return new Promise<request.Response>((resolve: (value: request.Response) => void, reject: (reason: any) => void): void => {
+  public static delete<T>(url: string): Promise<AJAXResponse<T>> {
+    return new Promise<AJAXResponse<T>>((resolve: (value: AJAXResponse<T>) => void, reject: (reason: any) => void): void => {
       request.del(url).end((err: any, res: request.Response) => {
           if (err) {
             reject(err);
             return;
           }
 
-          resolve(res);
+          const {
+            status,
+            body
+          } = res;
+
+          resolve({
+            status,
+            body
+          });
         });
     });
   }
