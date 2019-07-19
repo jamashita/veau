2ef8c22b35f7997fcf4020b62d59bf4c21b1defe
jamashita <jamashita@arachnid.xyz>
2019-07-19 09:39:05 +0900

    UpdatedAt

---
+++ src/veau-vo/UpdatedAt.ts
@@ -0,0 +1,50 @@
+import * as moment from 'moment';
+import { RuntimeError } from '../veau-error/RuntimeError';
+import { Type } from '../veau-general/Type';
+import { ValueObject } from './ValueObject';
+
+const TERM_FORMAT: string = 'YYYY-MM-DD HH:mm:ss';
+
+export class UpdatedAt extends ValueObject {
+  private at: moment.Moment;
+
+  public static of(at: moment.Moment): UpdatedAt {
+    return new UpdatedAt(at);
+  }
+
+  public static ofString(at: string): UpdatedAt {
+    if (!Type.isDateString(at)) {
+      throw new RuntimeError('AT IS NOT DATE FORMAT');
+    }
+
+    return UpdatedAt.of(moment(at));
+  }
+
+  private constructor(at: moment.Moment) {
+    super();
+    this.at = at;
+  }
+
+  public get(): moment.Moment {
+    return this.at;
+  }
+
+  public getString(): string {
+    return this.at.format(TERM_FORMAT);
+  }
+
+  public equals(other: UpdatedAt): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (this.at.isSame(other.get())) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public toString(): string {
+    return this.getString();
+  }
+}
---
+++ src/veau-vo/__tests__/UpdatedAt.spec.ts
@@ -0,0 +1,32 @@
+import 'jest';
+import * as moment from 'moment';
+import { RuntimeError } from '../../veau-error/RuntimeError';
+import { UpdatedAt } from '../UpdatedAt';
+
+describe('UpdatedAt', () => {
+  describe('equals', () => {
+    it('returns true if both properties are the same', () => {
+      const at1: UpdatedAt = UpdatedAt.of(moment('2000-01-01'));
+      const at2: UpdatedAt = UpdatedAt.of(moment('2000-01-02'));
+      const at3: UpdatedAt = UpdatedAt.of(moment('2000-01-01'));
+
+      expect(at1.equals(at1)).toEqual(true);
+      expect(at1.equals(at2)).toEqual(false);
+      expect(at1.equals(at3)).toEqual(true);
+    });
+  });
+
+  describe('ofString', () => {
+    it('throws error if the paramter is not date format', () => {
+      expect(() => {
+        UpdatedAt.ofString('this is not date');
+      }).toThrow(RuntimeError);
+    });
+
+    it('normal case', () => {
+      expect(() => {
+        UpdatedAt.ofString('2000-01-01');
+      }).not.toThrow(RuntimeError);
+    });
+  });
+});
