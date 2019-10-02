601e96eed42cae0dd36d1bf50cb740f2df22ccaf
jamashita <jamashita@arachnid.xyz>
2019-10-03 04:34:24 +0900

    type improvement

--- src/veau-general/Type/PlainObject.ts
+++ src/veau-general/Type/PlainObject.ts
@@ -1,5 +1,5 @@
 import { Primitive } from './Primitive';
 
 export type PlainObject = {
-  [name: string]: Primitive | PlainObject | Array<Primitive> | Array<PlainObject>;
+  [name: string]: Primitive | PlainObject | Array<Primitive | PlainObject>;
 };
