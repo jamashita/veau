c427252dd3e14e61598d965edd267321f01f26f6
jamashita <jamashita@arachnid.xyz>
2019-10-05 20:17:48 +0900

    failed test recovery

--- src/veau-general/MySQL/QueryMock.ts
+++ src/veau-general/MySQL/QueryMock.ts
@@ -2,7 +2,7 @@ import { IQuery } from './IQuery';
 
 export class QueryMock implements IQuery {
 
-  public execute(): Promise<void> {
-    return Promise.resolve<void>(undefined);
+  public execute<T>(): Promise<T> {
+    return Promise.resolve<T>(null as unknown as T);
   }
 }
