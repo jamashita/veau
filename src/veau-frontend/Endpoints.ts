f456abdeb0ca556a41194b260f132492b3156dc9
jamashita <jamashita@arachnid.xyz>
2019-10-15 21:48:39 +0900

    type hinting on Endpoints

--- src/veau-frontend/Endpoints.ts
+++ src/veau-frontend/Endpoints.ts
@@ -1,4 +1,10 @@
-export const Endpoints: {[key: string]: string} = {
+export const Endpoints: {
+  ENTRANCE: string;
+  STATS_LIST: string;
+  STATS_EDIT: string;
+  LOGOUT: string;
+  NOTFOUND: string;
+} = {
   ENTRANCE: '/',
   STATS_LIST: '/statistics/list',
   STATS_EDIT: '/statistics/edit/:id',
