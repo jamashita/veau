55aedb8dc10c0dae7054d1f872bcd8236d97f728
jamashita <jamashita@arachnid.xyz>
2019-11-01 10:13:30 +0900

    Password

---
+++ src/veau-vo/Password.ts
@@ -0,0 +1,45 @@
+import { ValueObject } from '../veau-general/ValueObject';
+
+export class Password extends ValueObject {
+  private password: string;
+
+  public static of(password: string): Password {
+    return new Password(password);
+  }
+
+  public static default(): Password {
+    return Password.of('');
+  }
+
+  private constructor(password: string) {
+    super();
+    this.password = password;
+  }
+
+  public get(): string {
+    return this.password;
+  }
+
+  public isDefault(): boolean {
+    if (this.password === '') {
+      return true;
+    }
+
+    return false;
+  }
+
+  public equals(other: Password): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (this.password === other.get()) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public toString(): string {
+    return this.password;
+  }
+}
---
+++ src/veau-vo/__tests__/Password.spec.ts
@@ -0,0 +1,34 @@
+import 'jest';
+import { Password } from '../Password';
+
+describe('Password', () => {
+  describe('equals', () => {
+    it('returns true if both properties are the same', () => {
+      const password1: Password = Password.of('password 1');
+      const password2: Password = Password.of('password 2');
+      const password3: Password = Password.of('password 1');
+
+      expect(password1.equals(password1)).toEqual(true);
+      expect(password1.equals(password2)).toEqual(false);
+      expect(password1.equals(password3)).toEqual(true);
+    });
+  });
+
+  describe('isDefault', () => {
+    it('retunrns true if the passwrord is empty string', () => {
+      const password1: Password = Password.default();
+      const password2: Password = Password.of('');
+      const password3: Password = Password.of('s');
+
+      expect(password1.isDefault()).toEqual(true);
+      expect(password2.isDefault()).toEqual(true);
+      expect(password3.isDefault()).toEqual(false);
+    });
+  });
+
+  describe('default', () => {
+    it('must be an blank password', () => {
+      expect(Password.default().get()).toEqual('');
+    });
+  });
+});
