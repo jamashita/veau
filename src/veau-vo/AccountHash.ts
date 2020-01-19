70049ee24da066c3290fad52b9eb8b502026ecbc
jamashita <jamashita@arachnid.xyz>
2020-01-20 05:04:50 +0900

    AccountHash

---
+++ src/veau-vo/AccountHash.ts
@@ -0,0 +1,55 @@
+import { ValueObject } from '../veau-general/ValueObject';
+import { Hash } from './Hash';
+import { VeauAccountID } from './VeauAccountID';
+
+export class AccountHash extends ValueObject {
+  private veauAccountID: VeauAccountID;
+  private hash: Hash;
+
+  public static of(veauAccountID: VeauAccountID, hash: Hash): AccountHash {
+    return new AccountHash(veauAccountID, hash);
+  }
+
+  private constructor(veauAccountID: VeauAccountID, hash: Hash) {
+    super();
+    this.veauAccountID = veauAccountID;
+    this.hash = hash;
+  }
+
+  public getVeauAccountID(): VeauAccountID {
+    return this.veauAccountID;
+  }
+
+  public getHash(): Hash {
+    return this.hash;
+  }
+
+  public equals(other: AccountHash): boolean {
+    if (this === other) {
+      return true;
+    }
+
+    const {
+      veauAccountID,
+      hash
+    } = this;
+
+    if (!veauAccountID.equals(other.getVeauAccountID())) {
+      return false;
+    }
+    if (!hash.equals(other.getHash())) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public toString(): string {
+    const {
+      veauAccountID,
+      hash
+    } = this;
+
+    return `${veauAccountID.toString()} ${hash.toString()}`;
+  }
+}
---
+++ src/veau-vo/__tests__/AccountHash.spec.ts
@@ -0,0 +1,40 @@
+import 'jest';
+import { AccountHash } from '../AccountHash';
+import { Hash } from '../Hash';
+import { VeauAccountID } from '../VeauAccountID';
+
+describe('AccountHash', () => {
+  describe('equals', () => {
+    it('returns true if all the properties are the same', () => {
+      const account1: VeauAccountID = VeauAccountID.of('998106de-b2e7-4981-9643-22cd30cd74de');
+      const account2: VeauAccountID = VeauAccountID.of('ee49aef0-b515-4fd8-9c4b-5ad9740ef4f9');
+      const hash1: Hash = Hash.of('hash 1');
+      const hash2: Hash = Hash.of('hash 2');
+
+      const accountHash1: AccountHash = AccountHash.of(account1, hash1);
+      const accountHash2: AccountHash = AccountHash.of(account2, hash1);
+      const accountHash3: AccountHash = AccountHash.of(account1, hash2);
+      const accountHash4: AccountHash = AccountHash.of(account2, hash2);
+      const accountHash5: AccountHash = AccountHash.of(account1, hash1);
+
+      expect(accountHash1.equals(accountHash1)).toEqual(true);
+      expect(accountHash1.equals(accountHash2)).toEqual(false);
+      expect(accountHash1.equals(accountHash3)).toEqual(false);
+      expect(accountHash1.equals(accountHash4)).toEqual(false);
+      expect(accountHash1.equals(accountHash5)).toEqual(true);
+    });
+  });
+
+  describe('toString', () => {
+    it('normal case', () => {
+      const id: string = '998106de-b2e7-4981-9643-22cd30cd74de';
+      const h: string = 'hash';
+      const account: VeauAccountID = VeauAccountID.of(id);
+      const hash: Hash = Hash.of(h);
+
+      const accountHash: AccountHash = AccountHash.of(account, hash);
+
+      expect(accountHash.toString()).toEqual(`${id} ${h}`);
+    });
+  });
+});
