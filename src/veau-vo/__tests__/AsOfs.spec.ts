6470ae18a491efa1bd36035b13d31ffed7976abd
jamashita <jamashita@arachnid.xyz>
2020-04-04 16:01:36 +0900

    AsOfs

--- src/veau-vo/__tests__/AsOfs.spec.ts
+++ src/veau-vo/__tests__/AsOfs.spec.ts
@@ -7,9 +7,9 @@ import { AsOfs } from '../AsOfs';
 describe('AsOfs', () => {
   describe('add', () => {
     it('does not affect the original one', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-01');
-      const asOf2: AsOf = AsOf.ofString('2000-01-02');
-      const asOf3: AsOf = AsOf.ofString('2000-01-03');
+      const asOf1: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-02').get();
+      const asOf3: AsOf = AsOf.ofString('2000-01-03').get();
 
       const asOfs1: AsOfs = AsOfs.of([asOf1, asOf2]);
       const asOfs2: AsOfs = asOfs1.add(asOf3);
@@ -27,9 +27,9 @@ describe('AsOfs', () => {
 
   describe('get', () => {
     it('returns AsOf instance at the correct index', ()  => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-01');
-      const asOf2: AsOf = AsOf.ofString('2000-01-02');
-      const asOf3: AsOf = AsOf.ofString('2000-01-03');
+      const asOf1: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-02').get();
+      const asOf3: AsOf = AsOf.ofString('2000-01-03').get();
 
       const asOfs: AsOfs = AsOfs.of([asOf1, asOf2, asOf3]);
 
@@ -53,10 +53,10 @@ describe('AsOfs', () => {
 
   describe('contains', () => {
     it('returns true if the element exists in the AsOfs', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-01');
-      const asOf2: AsOf = AsOf.ofString('2000-01-02');
-      const asOf3: AsOf = AsOf.ofString('2000-01-03');
-      const asOf4: AsOf = AsOf.ofString('2000-01-02');
+      const asOf1: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-02').get();
+      const asOf3: AsOf = AsOf.ofString('2000-01-03').get();
+      const asOf4: AsOf = AsOf.ofString('2000-01-02').get();
 
       const asOfs: AsOfs = AsOfs.of([asOf1, asOf2]);
 
@@ -69,10 +69,10 @@ describe('AsOfs', () => {
 
   describe('min', () => {
     it('returns minimal asOf', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-03');
-      const asOf2: AsOf = AsOf.ofString('2000-01-01');
-      const asOf3: AsOf = AsOf.ofString('2000-01-02');
-      const asOf4: AsOf = AsOf.ofString('2000-01-01');
+      const asOf1: AsOf = AsOf.ofString('2000-01-03').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf3: AsOf = AsOf.ofString('2000-01-02').get();
+      const asOf4: AsOf = AsOf.ofString('2000-01-01').get();
 
       const asOfs: AsOfs = AsOfs.of([asOf1, asOf2, asOf3, asOf4]);
 
@@ -88,10 +88,10 @@ describe('AsOfs', () => {
 
   describe('max', () => {
     it('returns minimal asOf', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-03');
-      const asOf2: AsOf = AsOf.ofString('2000-01-01');
-      const asOf3: AsOf = AsOf.ofString('2000-01-02');
-      const asOf4: AsOf = AsOf.ofString('2000-01-03');
+      const asOf1: AsOf = AsOf.ofString('2000-01-03').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf3: AsOf = AsOf.ofString('2000-01-02').get();
+      const asOf4: AsOf = AsOf.ofString('2000-01-03').get();
 
       const asOfs: AsOfs = AsOfs.of([asOf1, asOf2, asOf3, asOf4]);
 
@@ -107,8 +107,8 @@ describe('AsOfs', () => {
 
   describe('isEmpty', () => {
     it('returns true if the elements are 0', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-01');
-      const asOf2: AsOf = AsOf.ofString('2000-01-02');
+      const asOf1: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-02').get();
 
       const asOfs1: AsOfs = AsOfs.empty();
       const asOfs2: AsOfs = AsOfs.of([asOf1, asOf2]);
@@ -120,8 +120,8 @@ describe('AsOfs', () => {
 
   describe('equals', () => {
     it('returns false if the length is different', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-01');
-      const asOf2: AsOf = AsOf.ofString('2000-01-02');
+      const asOf1: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-02').get();
 
       const asOfs1: AsOfs = AsOfs.of([asOf1]);
       const asOfs2: AsOfs = AsOfs.of([asOf1, asOf2]);
@@ -131,8 +131,8 @@ describe('AsOfs', () => {
     });
 
     it('returns false if the sequence is different', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-01');
-      const asOf2: AsOf = AsOf.ofString('2000-01-02');
+      const asOf1: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-02').get();
 
       const asOfs1: AsOfs = AsOfs.of([asOf2, asOf1]);
       const asOfs2: AsOfs = AsOfs.of([asOf1, asOf2]);
@@ -142,8 +142,8 @@ describe('AsOfs', () => {
     });
 
     it('returns true if the length is the same and the sequence is the same', () => {
-      const asOf1: AsOf = AsOf.ofString('2000-01-01');
-      const asOf2: AsOf = AsOf.ofString('2000-01-02');
+      const asOf1: AsOf = AsOf.ofString('2000-01-01').get();
+      const asOf2: AsOf = AsOf.ofString('2000-01-02').get();
 
       const asOfs1: AsOfs = AsOfs.of([asOf1, asOf2]);
       const asOfs2: AsOfs = AsOfs.of([asOf1, asOf2]);
@@ -156,9 +156,9 @@ describe('AsOfs', () => {
   describe('toJSON', () => {
     it('normal case', () => {
       const asOfs: AsOfs = AsOfs.of([
-        AsOf.ofString('2000-01-01'),
-        AsOf.ofString('2000-01-02'),
-        AsOf.ofString('2000-01-03')
+        AsOf.ofString('2000-01-01').get(),
+        AsOf.ofString('2000-01-02').get(),
+        AsOf.ofString('2000-01-03').get()
       ]);
 
       expect(asOfs.toJSON()).toEqual([
@@ -172,9 +172,9 @@ describe('AsOfs', () => {
   describe('toString', () => {
     it('normal case', () => {
       const asOfs: AsOfs = AsOfs.of([
-        AsOf.ofString('2000-01-01'),
-        AsOf.ofString('2000-01-02'),
-        AsOf.ofString('2000-01-03')
+        AsOf.ofString('2000-01-01').get(),
+        AsOf.ofString('2000-01-02').get(),
+        AsOf.ofString('2000-01-03').get()
       ]);
 
       expect(asOfs.toString()).toEqual('2000-01-01, 2000-01-02, 2000-01-03');
