c15fdf6287a67deb5ab768e47b5885c4cb0d8795
jamashita <jamashita@arachnid.xyz>
2020-04-07 15:49:30 +0900

    RegionQuery

--- src/veau-query/MySQL/__tests__/RegionQuery.spec.ts
+++ src/veau-query/MySQL/__tests__/RegionQuery.spec.ts
@@ -3,12 +3,13 @@ import sinon, { SinonSpy, SinonStub } from 'sinon';
 import { kernel } from '../../../veau-container/Container';
 import { TYPE } from '../../../veau-container/Types';
 import { NoSuchElementError } from '../../../veau-error/NoSuchElementError';
+import { DataSourceError } from '../../../veau-general/DataSourceError';
 import { MockMySQL } from '../../../veau-general/MySQL/mocks/MockMySQL';
 import { MockMySQLError } from '../../../veau-general/MySQL/mocks/MockMySQLError';
 import { MySQLError } from '../../../veau-general/MySQL/MySQLError';
 import { Try } from '../../../veau-general/Try/Try';
 import { ISO3166 } from '../../../veau-vo/ISO3166';
-import { Region } from '../../../veau-vo/Region';
+import { Region, RegionRow } from '../../../veau-vo/Region';
 import { Regions } from '../../../veau-vo/Regions';
 import { RegionQuery } from '../RegionQuery';
 
@@ -25,10 +26,7 @@ describe('RegionQuery', () => {
 
   describe('all', () => {
     it('normal case', async () => {
-      const mysql: MockMySQL = new MockMySQL();
-      const stub: SinonStub = sinon.stub();
-      mysql.execute = stub;
-      stub.resolves([
+      const rows: Array<RegionRow> = [
         {
           regionID: 1,
           name: 'Afghanistan',
@@ -39,12 +37,16 @@ describe('RegionQuery', () => {
           name: 'Albania',
           iso3166: 'ALB'
         }
-      ]);
+      ]
+
+      const mysql: MockMySQL = new MockMySQL();
+      const stub: SinonStub = sinon.stub();
+      mysql.execute = stub;
+      stub.resolves(rows);
 
       const regionQuery: RegionQuery = new RegionQuery(mysql);
-      const trial: Try<Regions, NoSuchElementError | MySQLError> = await regionQuery.all();
+      const trial: Try<Regions, NoSuchElementError | DataSourceError> = await regionQuery.all();
 
-      expect(trial.isSuccess()).toEqual(true);
       expect(stub.withArgs(`SELECT
       R1.region_id AS regionID,
       R1.name,
@@ -52,14 +54,14 @@ describe('RegionQuery', () => {
       FROM regions R1
       FORCE INDEX(iso3166)
       ORDER BY R1.iso3166;`).called).toEqual(true);
+      expect(trial.isSuccess()).toEqual(true);
       const regions: Regions = trial.get();
       expect(regions.size()).toEqual(2);
-      expect(regions.get(0).get().getRegionID().get()).toEqual(1);
-      expect(regions.get(0).get().getName().get()).toEqual('Afghanistan');
-      expect(regions.get(0).get().getISO3166().get()).toEqual('AFG');
-      expect(regions.get(1).get().getRegionID().get()).toEqual(2);
-      expect(regions.get(1).get().getName().get()).toEqual('Albania');
-      expect(regions.get(1).get().getISO3166().get()).toEqual('ALB');
+      for (let i: number = 0; i < regions.size(); i++) {
+        expect(regions.get(i).get().getRegionID().get()).toEqual(rows[i].regionID);
+        expect(regions.get(i).get().getName().get()).toEqual(rows[i].name);
+        expect(regions.get(i).get().getISO3166().get()).toEqual(rows[i].iso3166);
+      }
     });
 
     it('returns Failure when MySQL.execute returns 0 results', async () => {
@@ -72,12 +74,12 @@ describe('RegionQuery', () => {
       const spy2: SinonSpy = sinon.spy();
 
       const regionQuery: RegionQuery = new RegionQuery(mysql);
-      const trial: Try<Regions, NoSuchElementError | MySQLError> = await regionQuery.all();
+      const trial: Try<Regions, NoSuchElementError | DataSourceError> = await regionQuery.all();
 
       expect(trial.isFailure()).toEqual(true);
       trial.match<void>(() => {
         spy1();
-      }, (err: NoSuchElementError | MySQLError) => {
+      }, (err: NoSuchElementError | DataSourceError) => {
         spy2();
         expect(err).toBeInstanceOf(NoSuchElementError);
       });
@@ -95,12 +97,12 @@ describe('RegionQuery', () => {
       const spy2: SinonSpy = sinon.spy();
 
       const regionQuery: RegionQuery = new RegionQuery(mysql);
-      const trial: Try<Regions, NoSuchElementError | MySQLError> = await regionQuery.all();
+      const trial: Try<Regions, NoSuchElementError | DataSourceError> = await regionQuery.all();
 
       expect(trial.isFailure()).toEqual(true);
       trial.match<void>(() => {
         spy1();
-      }, (err: NoSuchElementError | MySQLError) => {
+      }, (err: NoSuchElementError | DataSourceError) => {
         spy2();
         expect(err).toBeInstanceOf(MySQLError);
       });
@@ -136,21 +138,22 @@ describe('RegionQuery', () => {
 
   describe('findByISO3166', () => {
     it('normal case', async () => {
-      const mysql: MockMySQL = new MockMySQL();
-      const stub: SinonStub = sinon.stub();
-      mysql.execute = stub;
-      stub.resolves([
+      const rows: Array<RegionRow> = [
         {
           regionID: 2,
           name: 'Albania',
           iso3166: 'ALB'
         }
-      ]);
+      ];
+
+      const mysql: MockMySQL = new MockMySQL();
+      const stub: SinonStub = sinon.stub();
+      mysql.execute = stub;
+      stub.resolves(rows);
 
       const regionQuery: RegionQuery = new RegionQuery(mysql);
-      const trial: Try<Region, NoSuchElementError | MySQLError> = await regionQuery.findByISO3166(ISO3166.of('ALB'));
+      const trial: Try<Region, NoSuchElementError | DataSourceError> = await regionQuery.findByISO3166(ISO3166.of('ALB'));
 
-      expect(trial.isSuccess()).toEqual(true);
       expect(stub.withArgs(`SELECT
       R1.region_id AS regionID,
       R1.name,
@@ -159,10 +162,11 @@ describe('RegionQuery', () => {
       WHERE R1.iso3166 = :iso3166;`, {
         iso3166: 'ALB'
       }).called).toEqual(true);
+      expect(trial.isSuccess()).toEqual(true);
       const region: Region = trial.get();
-      expect(region.getRegionID().get()).toEqual(2);
-      expect(region.getName().get()).toEqual('Albania');
-      expect(region.getISO3166().get()).toEqual('ALB');
+      expect(region.getRegionID().get()).toEqual(rows[0].regionID);
+      expect(region.getName().get()).toEqual(rows[0].name);
+      expect(region.getISO3166().get()).toEqual(rows[0].iso3166);
     });
 
     it('returns Failure because MySQL returns 0 results', async () => {
@@ -175,12 +179,12 @@ describe('RegionQuery', () => {
       const spy2: SinonSpy = sinon.spy();
 
       const regionQuery: RegionQuery = new RegionQuery(mysql);
-      const trial: Try<Region, NoSuchElementError | MySQLError> = await regionQuery.findByISO3166(ISO3166.of('ALB'));
+      const trial: Try<Region, NoSuchElementError | DataSourceError> = await regionQuery.findByISO3166(ISO3166.of('ALB'));
 
       expect(trial.isFailure()).toEqual(true);
       trial.match<void>(() => {
         spy1();
-      }, (err: NoSuchElementError | MySQLError) => {
+      }, (err: NoSuchElementError | DataSourceError) => {
         spy2();
         expect(err).toBeInstanceOf(NoSuchElementError);
       });
@@ -198,12 +202,12 @@ describe('RegionQuery', () => {
       const spy2: SinonSpy = sinon.spy();
 
       const regionQuery: RegionQuery = new RegionQuery(mysql);
-      const trial: Try<Region, NoSuchElementError | MySQLError> = await regionQuery.findByISO3166(ISO3166.of('ALB'));
+      const trial: Try<Region, NoSuchElementError | DataSourceError> = await regionQuery.findByISO3166(ISO3166.of('ALB'));
 
       expect(trial.isFailure()).toEqual(true);
       trial.match<void>(() => {
         spy1();
-      }, (err: NoSuchElementError | MySQLError) => {
+      }, (err: NoSuchElementError | DataSourceError) => {
         spy2();
         expect(err).toBeInstanceOf(MySQLError);
       });
