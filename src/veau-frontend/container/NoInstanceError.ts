acf93a28639b4c0487ec4d3d55b9906bd8341b99
jamashita <jamashita@arachnid.xyz>
2020-04-07 11:44:35 +0900

    container deleted

--- src/veau-frontend/container/NoInstanceError.ts
+++
@@ -1,9 +0,0 @@
-import { RuntimeError } from '../../veau-general/RuntimeError';
-
-export class NoInstanceError extends RuntimeError {
-  public readonly name: 'NoInstanceError' = 'NoInstanceError';
-
-  public constructor(message: string) {
-    super(message);
-  }
-}
