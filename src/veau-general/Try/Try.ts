2ae0b609e49764bd9a5e220ee88164a6292a75c5
jamashita <jamashita@arachnid.xyz>
2020-04-08 16:25:48 +0900

    remocve unneccesary ones

--- src/veau-general/Try/Try.ts
+++ src/veau-general/Try/Try.ts
@@ -8,10 +8,6 @@ export abstract class Try<S, F extends Error> {
   protected constructor() {
   }
 
-  // public static all(): Try<any, any> {
-  //   //
-  // }
-
   public abstract get(): S;
 
   public abstract isSuccess(): this is Success<S, F>;
--- src/veau-general/Type/Value.ts
+++ src/veau-general/Type/Value.ts
@@ -2,9 +2,8 @@ export type Primitive = null | undefined | number | string | boolean;
 export type Nullable<T> = T | null;
 export type Ambiguous<T>  = T | undefined;
 export type Suspicious<T> = T | null | undefined;
-type Value = Primitive | PlainObject;
 export type PlainObject = {
-  [name: string]: Value | Array<Value>;
+  [name: string]: Item | Array<Item>;
 };
 type Item = Primitive | PlainObject;
 export type JSObjectNotation = Item | Array<Item>;
