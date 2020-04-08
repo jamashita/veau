727c6bae78811e4b6997a560a6913515e2b2a89b
jamashita <jamashita@arachnid.xyz>
2020-04-09 04:57:42 +0900

    move

--- src/veau-general/MockError.ts
+++ src/veau-general/MockError.ts
@@ -1,4 +1,4 @@
-import { RuntimeError } from '../RuntimeError';
+import { RuntimeError } from './RuntimeError';
 
 export class MockError extends RuntimeError {
   public readonly name: 'MockError' = 'MockError';
