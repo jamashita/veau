8d95295281e7f9e2bccfc28541779d3c99d7a7f8
jamashita <jamashita@yamashita.xyz>
2020-04-12 01:31:37 +0900

    code formmting

--- src/veau-vo/Terms.ts
+++ src/veau-vo/Terms.ts
@@ -1,28 +1,38 @@
-import { Collection } from '../veau-general/Collection/Collection';
-import { Sequence } from '../veau-general/Collection/Sequence';
-import { Optional } from '../veau-general/Optional/Optional';
-import { Mapper } from '../veau-general/Type/Function';
-import { Term } from './Term';
+import {Collection} from '../veau-general/Collection/Collection';
+import {Sequence} from '../veau-general/Collection/Sequence';
+import {Optional} from '../veau-general/Optional/Optional';
+import {Mapper} from '../veau-general/Type/Function';
+import {Term} from './Term';
 
 export class Terms implements Collection<number, Term> {
   public readonly noun: 'Terms' = 'Terms';
   private readonly terms: Sequence<Term>;
 
+  public static readonly ALL: Terms = Terms.ofSpread(
+    Term.DAILY,
+    Term.WEEKLY,
+    Term.MONTHLY,
+    Term.QUARTERLY,
+    Term.ANNUAL
+  );
+
   public static all(): Terms {
-    return Terms.of(Sequence.of<Term>([
-      Term.DAILY,
-      Term.WEEKLY,
-      Term.MONTHLY,
-      Term.QUARTERLY,
-      Term.ANNUAL
-    ]));
+    return Terms.ALL;
   }
 
-  private static of(terms: Sequence<Term>): Terms {
+  protected static of(terms: Sequence<Term>): Terms {
     return new Terms(terms);
   }
 
-  private constructor(terms: Sequence<Term>) {
+  protected static ofArray(terms: Array<Term>): Terms {
+    return Terms.of(Sequence.of<Term>(terms));
+  }
+
+  protected static ofSpread(...terms: Array<Term>): Terms {
+    return Terms.ofArray(terms);
+  }
+
+  protected constructor(terms: Sequence<Term>) {
     this.terms = terms;
   }
 
