924f80d13f8ac253d18521ea317abd2f6123d774
jamashita <jamashita@yamashita.xyz>
2020-04-12 23:53:43 +0900

    renaming

--- src/Command/Cache/LocaleCommand.ts
+++ src/Command/Cache/LocaleCommand.ts
@@ -15,15 +15,15 @@ import { ILocaleCommand } from '../Interface/ILocaleCommand';
 export class LocaleCommand implements ILocaleCommand, ICacheCommand {
   public readonly noun: 'LocaleCommand' = 'LocaleCommand';
   public readonly source: 'Cache' = 'Cache';
-  private readonly heap: ICache;
+  private readonly cache: ICache;
 
-  public constructor(@inject(TYPE.Vault) heap: ICache) {
-    this.heap = heap;
+  public constructor(@inject(TYPE.Vault) cache: ICache) {
+    this.cache = cache;
   }
 
   public create(locale: Locale): Promise<Try<void, DataSourceError>> {
     try {
-      this.heap.set(VAULT_LOCALE_KEY, locale);
+      this.cache.set(VAULT_LOCALE_KEY, locale);
 
       // eslint-disable-next-line @typescript-eslint/return-await
       return Promise.resolve<Try<void, DataSourceError>>(Success.of<DataSourceError>());
