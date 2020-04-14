79799171207a9d32d475af566db98f8c757e53e0
jamashita <jamashita@yamashita.xyz>
2020-04-15 00:21:32 +0900

    done sign

--- src/Interactor/__tests__/AuthenticationInteractor.spec.ts
+++ src/Interactor/__tests__/AuthenticationInteractor.spec.ts
@@ -14,6 +14,7 @@ import { MockRegion } from '../../VO/Mock/MockRegion';
 import { VeauAccount } from '../../VO/VeauAccount';
 import { AuthenticationInteractor } from '../AuthenticationInteractor';
 
+// ODNE
 describe('AuthenticationInteractor', () => {
   describe('container', () => {
     it('must be a singleton', () => {
