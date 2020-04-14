9af2fe2f3e8daea1e78c65830dc2cdf7c3567f40
jamashita <jamashita@yamashita.xyz>
2020-04-14 18:33:30 +0900

    new mock

---
+++ src/VO/Mock/MockVeauAccount.ts
@@ -0,0 +1,28 @@
+import { AccountName } from '../AccountName';
+import { Language } from '../Language';
+import { Region } from '../Region';
+import { VeauAccount } from '../VeauAccount';
+import { VeauAccountID } from '../VeauAccountID';
+import { MockAccountName } from './MockAccountName';
+import { MockLanguage } from './MockLanguage';
+import { MockRegion } from './MockRegion';
+import { MockVeauAccountID } from './MockVeauAccountID';
+
+type VeauAccountArgs = Partial<Readonly<{
+  veauAccountID: VeauAccountID;
+  account: AccountName;
+  language: Language;
+  region: Region;
+}>>;
+
+export class MockVeauAccount extends VeauAccount {
+
+  protected constructor({
+    veauAccountID = new MockVeauAccountID(),
+    account = new MockAccountName(),
+    language = new MockLanguage(),
+    region = new MockRegion()
+  }: VeauAccountArgs = {}) {
+    super(veauAccountID, account, language, region);
+  }
+}
