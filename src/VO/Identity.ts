b0ebc9ff941296f9f37008657492f62940076e54
jamashita <jamashita@arachnid.xyz>
2020-05-07 02:27:51 +0900

    Idenitty

---
+++ src/VO/Identity.ts
@@ -0,0 +1,91 @@
+import { ValueObject } from 'publikum';
+import { AccountName } from './AccountName';
+import { Language } from './Language';
+import { Region } from './Region';
+import { VeauAccountID } from './VeauAccountID';
+
+export class Identity extends ValueObject {
+  public readonly noun: 'VeauAccount' = 'VeauAccount';
+  private readonly veauAccountID: VeauAccountID;
+  private readonly name: AccountName;
+  private readonly language: Language;
+  private readonly region: Region;
+
+  public static of(
+    veauAccountID: VeauAccountID,
+    name: AccountName,
+    language: Language,
+    region: Region
+  ): Identity {
+    return new Identity(veauAccountID, name, language, region);
+  }
+
+  public static empty(): Identity {
+    return Identity.of(
+      VeauAccountID.generate(),
+      AccountName.empty(),
+      Language.empty(),
+      Region.empty()
+    );
+  }
+
+  protected constructor(
+    veauAccountID: VeauAccountID,
+    name: AccountName,
+    language: Language,
+    region: Region
+  ) {
+    super();
+    this.veauAccountID = veauAccountID;
+    this.name = name;
+    this.language = language;
+    this.region = region;
+  }
+
+  public getVeauAccountID(): VeauAccountID {
+    return this.veauAccountID;
+  }
+
+  public getAccount(): AccountName {
+    return this.name;
+  }
+
+  public getLanguage(): Language {
+    return this.language;
+  }
+
+  public getRegion(): Region {
+    return this.region;
+  }
+
+  public equals(other: Identity): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (!this.veauAccountID.equals(other.veauAccountID)) {
+      return false;
+    }
+    if (!this.name.equals(other.name)) {
+      return false;
+    }
+    if (!this.language.equals(other.language)) {
+      return false;
+    }
+    if (!this.region.equals(other.region)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public serialize(): string {
+    const properties: Array<string> = [];
+
+    properties.push(this.veauAccountID.toString());
+    properties.push(this.name.toString());
+    properties.push(this.language.toString());
+    properties.push(this.region.toString());
+
+    return properties.join(' ');
+  }
+}
