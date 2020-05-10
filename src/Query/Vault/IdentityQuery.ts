589f33e8d1cdaace5bc6c0151c69d0fa6d9bfbcd
jamashita <jamashita@arachnid.xyz>
2020-05-10 12:03:46 +0900

    findByEntranceInfo

--- src/Query/Vault/IdentityQuery.ts
+++ src/Query/Vault/IdentityQuery.ts
@@ -87,7 +87,50 @@ export class IdentityQuery implements IIdentityQuery, IVaultQuery {
   }
 
   public async findByEntranceInfo(entranceInformation: EntranceInformation): Promise<Superposition<Identity, IdentityError | DataSourceError>> {
-    throw new Error('Method not implemented.');
-  }
+    const superposition1: Superposition<VeauAccount, VeauAccountError | DataSourceError> = await this.veauAccountQuery.findByEntranceInfo(entranceInformation);
 
+    return superposition1.match<Superposition<Identity, IdentityError | DataSourceError>>(async (veauAccount: VeauAccount) => {
+      const [
+        superposition2,
+        superposition3
+      ]: [
+          Superposition<Language, LanguageError | NoSuchElementError | DataSourceError>,
+          Superposition<Region, RegionError | NoSuchElementError | DataSourceError>
+        ] = await Promise.all([
+          this.languageQuery.find(veauAccount.getLanguageID()),
+          this.regionQuery.find(veauAccount.getRegionID())
+        ])
+
+      return superposition2.match<Superposition<Identity, IdentityError | DataSourceError>>((language: Language) => {
+        return superposition3.match<Superposition<Identity, IdentityError | DataSourceError>>((region: Region) => {
+          return Alive.of<Identity, DataSourceError>(
+            Identity.of(
+              veauAccount.getVeauAccountID(),
+              veauAccount.getAccountName(),
+              language,
+              region
+            )
+          );
+        }, (err: RegionError | NoSuchElementError | DataSourceError) => {
+          if (err instanceof DataSourceError) {
+            return Dead.of<Identity, DataSourceError>(err);
+          }
+
+          return Dead.of<Identity, IdentityError>(new IdentityError('IdentityQuery.find()', err));
+        });
+      }, (err: LanguageError | NoSuchElementError | DataSourceError) => {
+        if (err instanceof DataSourceError) {
+          return Dead.of<Identity, DataSourceError>(err);
+        }
+
+        return Dead.of<Identity, IdentityError>(new IdentityError('IdentityQuery.find()', err));
+      });
+    }, (err: VeauAccountError | DataSourceError) => {
+      if (err instanceof VeauAccountError) {
+        return Promise.resolve<Superposition<Identity, IdentityError>>(Dead.of<Identity, IdentityError>(new IdentityError('IdentityQuery.find()', err)));
+      }
+
+      return Promise.resolve<Superposition<Identity, DataSourceError>>(Dead.of<Identity, DataSourceError>(err));
+    });
+  }
 }
