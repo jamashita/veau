a2b4605e67ddd77dbc77d2c95f1b43fd5c5f5970
jamashita <jamashita@arachnid.xyz>
2020-05-10 15:14:38 +0900

    containe

---
+++ src/Query/Vault/__tests__/IdentityQuery.spec.ts
@@ -0,0 +1,290 @@
+import { INTERNAL_SERVER_ERROR, OK, UNAUTHORIZED } from "http-status";
+import {
+  AJAXError,
+  DataSourceError,
+  MockAJAX,
+  Superposition,
+  UUID,
+} from "publikum";
+import "reflect-metadata";
+import sinon, { SinonSpy, SinonStub } from "sinon";
+import { TYPE } from "../../../Container/Types";
+import { vault } from "../../../Container/Vault";
+import { VeauAccountError } from "../../../Error/VeauAccountError";
+import { MockAccountName } from "../../../VO/Mock/MockAccountName";
+import { MockEntranceInformation } from "../../../VO/Mock/MockEntranceInformation";
+import { MockPassword } from "../../../VO/Mock/MockPassword";
+import { VeauAccount, VeauAccountJSON } from "../../../VO/VeauAccount";
+import { VeauAccountQuery } from "../VeauAccountQuery";
+
+describe("VeauAccountQuery", () => {
+  describe("container", () => {
+    it("must be a singleton", () => {
+      const veauAccountQuery1: VeauAccountQuery = vault.get<VeauAccountQuery>(
+        TYPE.VeauAccountAJAXQuery
+      );
+      const veauAccountQuery2: VeauAccountQuery = vault.get<VeauAccountQuery>(
+        TYPE.VeauAccountAJAXQuery
+      );
+
+      expect(veauAccountQuery1).toBeInstanceOf(VeauAccountQuery);
+      expect(veauAccountQuery1).toBe(veauAccountQuery2);
+    });
+  });
+
+  describe("find", () => {
+    it("normal case", async () => {
+      const json: VeauAccountJSON = {
+        veauAccountID: UUID.v4().get(),
+        languageID: UUID.v4().get(),
+        regionID: UUID.v4().get(),
+        name: "name",
+      };
+
+      const ajax: MockAJAX = new MockVeauAccountq();
+      const stub: SinonStub = sinon.stub();
+      ajax.get = stub;
+      stub.resolves({
+        status: OK,
+        body: json,
+      });
+
+      const VeauAccountQuery: VeauAccountQuery = new VeauAccountQuery(ajax);
+      const superposition: Superposition<
+        VeauAccount,
+        VeauAccountError | DataSourceError
+      > = await VeauAccountQuery.find();
+
+      expect(stub.withArgs("/api/identity").called).toBe(true);
+      expect(superposition.isAlive()).toBe(true);
+      const veauAccount: VeauAccount = superposition.get();
+      expect(veauAccount.getVeauAccountID().get().get()).toBe(
+        json.veauAccountID
+      );
+      expect(veauAccount.getLanguageID().get().get()).toBe(json.languageID);
+      expect(veauAccount.getRegionID().get().get()).toBe(json.regionID);
+      expect(veauAccount.getAccountName().get()).toBe(json.name);
+    });
+
+    it("returns Dead when it has wrong format veauAccountID", async () => {
+      const json: VeauAccountJSON = {
+        veauAccountID: "malformat uuid",
+        languageID: UUID.v4().get(),
+        regionID: UUID.v4().get(),
+        name: "name",
+      };
+
+      const ajax: MockAJAX = new MockAJAX();
+      const stub: SinonStub = sinon.stub();
+      ajax.get = stub;
+      stub.resolves({
+        status: OK,
+        body: json,
+      });
+      const spy1: SinonSpy = sinon.spy();
+      const spy2: SinonSpy = sinon.spy();
+
+      const VeauAccountQuery: VeauAccountQuery = new VeauAccountQuery(ajax);
+      const superposition: Superposition<
+        VeauAccount,
+        VeauAccountError | DataSourceError
+      > = await VeauAccountQuery.find();
+
+      expect(superposition.isDead()).toBe(true);
+      superposition.match<void>(
+        () => {
+          spy1();
+        },
+        (err: VeauAccountError | DataSourceError) => {
+          spy2();
+          expect(err).toBeInstanceOf(VeauAccountError);
+        }
+      );
+
+      expect(spy1.called).toBe(false);
+      expect(spy2.called).toBe(true);
+    });
+
+    it("doesn't return OK", async () => {
+      const ajax: MockAJAX = new MockAJAX();
+      const stub: SinonStub = sinon.stub();
+      ajax.get = stub;
+      stub.resolves({
+        status: INTERNAL_SERVER_ERROR,
+        body: {},
+      });
+      const spy1: SinonSpy = sinon.spy();
+      const spy2: SinonSpy = sinon.spy();
+
+      const VeauAccountQuery: VeauAccountQuery = new VeauAccountQuery(ajax);
+      const superposition: Superposition<
+        VeauAccount,
+        VeauAccountError | DataSourceError
+      > = await VeauAccountQuery.find();
+
+      expect(superposition.isDead()).toBe(true);
+      superposition.match<void>(
+        () => {
+          spy1();
+        },
+        (err: VeauAccountError | DataSourceError) => {
+          spy2();
+          expect(err).toBeInstanceOf(DataSourceError);
+        }
+      );
+
+      expect(spy1.called).toBe(false);
+      expect(spy2.called).toBe(true);
+    });
+  });
+
+  describe("findByEntranceInfo", () => {
+    it("normal case", async () => {
+      const json: VeauAccountJSON = {
+        veauAccountID: UUID.v4().get(),
+        languageID: UUID.v4().get(),
+        regionID: UUID.v4().get(),
+        name: "name",
+      };
+
+      const ajax: MockAJAX = new MockAJAX();
+      const stub: SinonStub = sinon.stub();
+      ajax.post = stub;
+      stub.resolves({
+        status: OK,
+        body: json,
+      });
+
+      const info: MockEntranceInformation = new MockEntranceInformation({
+        account: new MockAccountName("name"),
+        password: new MockPassword("password"),
+      });
+      const VeauAccountQuery: VeauAccountQuery = new VeauAccountQuery(ajax);
+      const superposition: Superposition<
+        VeauAccount,
+        VeauAccountError | DataSourceError
+      > = await VeauAccountQuery.findByEntranceInfo(info);
+
+      expect(
+        stub.withArgs("/api/auth", {
+          account: "name",
+          password: "password",
+        }).called
+      ).toBe(true);
+      expect(superposition.isAlive()).toBe(true);
+      const veauAccount: VeauAccount = superposition.get();
+      expect(veauAccount.getVeauAccountID().get().get()).toBe(
+        json.veauAccountID
+      );
+      expect(veauAccount.getLanguageID().get().get()).toBe(json.languageID);
+      expect(veauAccount.getRegionID().get().get()).toBe(json.regionID);
+      expect(veauAccount.getAccountName().get()).toBe(json.name);
+    });
+
+    it("returns Dead when it has wrong format veauAccountID", async () => {
+      const json: VeauAccountJSON = {
+        veauAccountID: "malformat uuid",
+        languageID: UUID.v4().get(),
+        regionID: UUID.v4().get(),
+        name: "name",
+      };
+
+      const ajax: MockAJAX = new MockAJAX();
+      const stub: SinonStub = sinon.stub();
+      ajax.post = stub;
+      stub.resolves({
+        status: OK,
+        body: json,
+      });
+      const spy1: SinonSpy = sinon.spy();
+      const spy2: SinonSpy = sinon.spy();
+
+      const info: MockEntranceInformation = new MockEntranceInformation();
+      const VeauAccountQuery: VeauAccountQuery = new VeauAccountQuery(ajax);
+      const superposition: Superposition<
+        VeauAccount,
+        VeauAccountError | DataSourceError
+      > = await VeauAccountQuery.findByEntranceInfo(info);
+
+      expect(superposition.isDead()).toBe(true);
+      superposition.match<void>(
+        () => {
+          spy1();
+        },
+        (err: VeauAccountError | DataSourceError) => {
+          spy2();
+          expect(err).toBeInstanceOf(VeauAccountError);
+        }
+      );
+
+      expect(spy1.called).toBe(false);
+      expect(spy2.called).toBe(true);
+    });
+
+    it("returns UNAUTHORIZED", async () => {
+      const ajax: MockAJAX = new MockAJAX();
+      const stub: SinonStub = sinon.stub();
+      ajax.post = stub;
+      stub.resolves({
+        status: UNAUTHORIZED,
+        body: {},
+      });
+      const spy1: SinonSpy = sinon.spy();
+      const spy2: SinonSpy = sinon.spy();
+
+      const info: MockEntranceInformation = new MockEntranceInformation();
+      const VeauAccountQuery: VeauAccountQuery = new VeauAccountQuery(ajax);
+      const superposition: Superposition<
+        VeauAccount,
+        VeauAccountError | DataSourceError
+      > = await VeauAccountQuery.findByEntranceInfo(info);
+
+      expect(superposition.isDead()).toBe(true);
+      superposition.match<void>(
+        () => {
+          spy1();
+        },
+        (err: VeauAccountError | DataSourceError) => {
+          spy2();
+          expect(err).toBeInstanceOf(DataSourceError);
+        }
+      );
+
+      expect(spy1.called).toBe(false);
+      expect(spy2.called).toBe(true);
+    });
+
+    it("doesn't return OK nor UNAUTHORIZED", async () => {
+      const ajax: MockAJAX = new MockAJAX();
+      const stub: SinonStub = sinon.stub();
+      ajax.post = stub;
+      stub.resolves({
+        status: INTERNAL_SERVER_ERROR,
+        body: {},
+      });
+      const spy1: SinonSpy = sinon.spy();
+      const spy2: SinonSpy = sinon.spy();
+
+      const info: MockEntranceInformation = new MockEntranceInformation();
+      const VeauAccountQuery: VeauAccountQuery = new VeauAccountQuery(ajax);
+      const superposition: Superposition<
+        VeauAccount,
+        VeauAccountError | DataSourceError
+      > = await VeauAccountQuery.findByEntranceInfo(info);
+
+      expect(superposition.isDead()).toBe(true);
+      superposition.match<void>(
+        () => {
+          spy1();
+        },
+        (err: VeauAccountError | DataSourceError) => {
+          spy2();
+          expect(err).toBeInstanceOf(AJAXError);
+        }
+      );
+
+      expect(spy1.called).toBe(false);
+      expect(spy2.called).toBe(true);
+    });
+  });
+});
