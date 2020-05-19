d7700a437ad41bed4d915e05952d2bd31f369892
jamashita <jamashita@arachnid.xyz>
2020-05-20 08:41:51 +0900

    change the reference fiel

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -30,7 +30,7 @@ module.exports = {
       }
     }),
     new TsConfigWebpackPlugin({
-      configFile: './src/Frontend/tsconfig.json'
+      configFile: './tsconfig-fe.json'
     })
   ],
   module: {
