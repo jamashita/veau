f93e090e8756e3455c70ab1ea2dc8b202cfc9a66
jamashita <jamashita@arachnid.xyz>
2020-06-11 00:31:29 +0900

    unnecessary middlewre

--- src/Controller/API/AuthController.ts
+++ src/Controller/API/AuthController.ts
@@ -4,14 +4,11 @@ import { injectable } from 'inversify';
 import passport from 'passport';
 import { Controller, Post, Res, UseBefore } from 'routing-controllers';
 
-import { AuthenticationMiddleware } from '../Middleware/AuthenticationMiddleware';
-
 @injectable()
 @Controller('/auth')
 export class AuthController {
   @Post('/')
   @UseBefore(passport.authenticate('local'))
-  @UseBefore(AuthenticationMiddleware)
   public auth(@Res() res: Response): Response {
     return res.status(OK).send(res.locals.account.toJSON());
   }
