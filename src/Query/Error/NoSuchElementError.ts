94124a0b331893848d2f0079eb14cb8db1b8e501
jamashita <jamashita@arachnid.xyz>
2020-06-29 02:50:36 +0900

    NoSuchElementError

--- src/Query/Error/NoSuchElementError.ts
+++ src/Query/Error/NoSuchElementError.ts
@@ -1,7 +1,7 @@
 import { RuntimeError } from '@jamashita/publikum-error';
 
-export class NoSuchElementError extends RuntimeError {
-  public readonly name: 'NoSuchElementError' = 'NoSuchElementError';
+export class NoSuchElementError extends RuntimeError<'NoSuchElementError'> {
+  public readonly noun: 'NoSuchElementError' = 'NoSuchElementError';
 
   public constructor(message: number | string, cause?: Error) {
     super(`NO SUCH KEY: ${message}`, cause);
