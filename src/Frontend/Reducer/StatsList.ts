54aff0aeabf03554a716c834e06405c6e5614ce4
jamashita <jamashita@arachnid.xyz>
2020-09-18 19:21:51 +0900

    redycer ypdate

--- src/Frontend/Reducer/StatsList.ts
+++ src/Frontend/Reducer/StatsList.ts
@@ -1,15 +1,12 @@
 import { Reducer } from 'redux';
 import { Stats } from '../../Entity/Stats/Stats';
-import { StatsDisplay } from '../../VO/Display/StatsDisplay';
 import { StatsListItems } from '../../VO/StatsListItem/StatsListItems';
 import {
   STATS_LIST_CLOSE_STATS_MODAL,
   STATS_LIST_OPEN_STATS_MODAL,
   STATS_LIST_RESET_STATS,
-  STATS_LIST_RESET_STATS_DISPLAY,
   STATS_LIST_RESET_STATS_ITEMS,
   STATS_LIST_UPDATE_STATS,
-  STATS_LIST_UPDATE_STATS_DISPLAY,
   STATS_LIST_UPDATE_STATS_ITEMS,
   VeauAction
 } from '../Action';
@@ -17,14 +14,12 @@ import {
 const initialState: StatsList = {
   open: false,
   stats: Stats.default(),
-  display: StatsDisplay.default(),
   items: StatsListItems.empty()
 };
 
 export type StatsList = Readonly<{
   open: boolean;
   stats: Stats;
-  display: StatsDisplay;
   items: StatsListItems;
 }>;
 
@@ -55,18 +50,6 @@ export const statsList: Reducer<StatsList, VeauAction> = (
       return {
         ...state,
         stats: Stats.default()
-      };
-    }
-    case STATS_LIST_UPDATE_STATS_DISPLAY: {
-      return {
-        ...state,
-        display: action.display
-      };
-    }
-    case STATS_LIST_RESET_STATS_DISPLAY: {
-      return {
-        ...state,
-        display: StatsDisplay.default()
       };
     }
     case STATS_LIST_UPDATE_STATS_ITEMS: {
