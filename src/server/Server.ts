ee648f6dd606aac9b3f0f1d3929d541e365500a0
jamashita <jamashita@arachnid.xyz>
2021-05-09 11:20:50 +0900

    feat: 🎸 new Server constuctuin

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,16 +1,16 @@
-import { Ambiguous, Kind, Nullable } from '@jamashita/anden-type';
+import { Ambiguous, Kind } from '@jamashita/anden-type';
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import config from 'config';
-import ffy, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import secureSession, { SecureSessionPluginOptions } from 'fastify-secure-session';
+import { FastifyPluginAsync } from 'fastify/types/plugin';
 import 'reflect-metadata';
+import { BaseController } from '../controller/BaseController';
 import { log } from '../infrastructure/Logger';
 
-const fastify: FastifyInstance = ffy({
-  logger: log
-});
-
 const port: number = config.get<number>('port');
-// eslint-disable-next-line no-process-env
 const mode: Ambiguous<string> = process.env['NODE_ENV'];
+// const session;
 
 if (Kind.isUndefined(mode)) {
   log.fatal('mode IS NOT SET');
@@ -22,65 +22,19 @@ process.on('unhandledRejection', (reason: unknown) => {
   log.fatal('UNHANDLED REJECTION', reason);
 });
 
-fastify.get('/:id', async (request: FastifyRequest, reply: FastifyReply) => {
-  await reply.send({
-    p: request.query,
-    q: request.params
+const bootstrap = async (): Promise<unknown> => {
+  const app: NestFastifyApplication = await NestFactory.create<NestFastifyApplication>(BaseController, new FastifyAdapter());
+
+  // @ts-ignore
+  await app.register(secureSession as FastifyPluginAsync<SecureSessionPluginOptions>, {
+    cookieName: 'VEAU',
+    secret: '',
+    salt: ''
   });
+
+  return app.listen(port);
+};
+
+bootstrap().then(() => {
+  log.info(`Server running on port ${port} in ${mode} mode`);
 });
-
-fastify.listen(port, (err: Nullable<Error>) => {
-  if (Kind.isNull(err)) {
-    log.info(`Server running on port ${port} in ${mode} mode`);
-
-    return;
-  }
-
-  log.error(err);
-  // eslint-disable-next-line node/no-process-exit,no-process-exit
-  process.exit(1);
-});
-
-// const app: Express = express();
-//
-// app.disable('x-powered-by');
-// app.set('views', path.resolve('static', 'views'));
-// app.set('view engine', 'pug');
-// app.use(
-//   express.urlencoded({
-//     extended: false
-//   })
-// );
-// app.use(express.json());
-// app.use(compression());
-// app.use(helmet());
-// app.use(express.static(path.resolve(__dirname, 'public')));
-// app.use(favicon(path.resolve('static', 'favicon.ico')));
-//
-// const RedisStore: connectRedis.RedisStore = connectRedis(expressSession);
-// const sessionStore: expressSession.Store = new RedisStore({
-//   client: veauRedis.getClient(),
-//   prefix: 'veau::'
-// });
-// const sessionMiddleware: RequestHandler = expressSession({
-//   secret: 'Ziuye5J4VmwxacL7dvV98dqUqT7HbfTn',
-//   store: sessionStore,
-//   resave: false,
-//   rolling: true,
-//   saveUninitialized: false,
-//   cookie: {
-//     httpOnly: true,
-//     secure: false,
-//     maxAge: 1000 * 60 * 60 * 8
-//   }
-// });
-//
-// app.use(sessionMiddleware);
-// app.use(passport.initialize());
-// app.use(passport.session());
-//
-// BaseController(app);
-//
-// app.listen(port, () => {
-//   logger.info(`Server running on port ${port} in ${mode} mode`);
-// });
